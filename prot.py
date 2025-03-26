5eb26a561c042ed4c9cc9fe62d52ef7d0b559ced
caiocoutinho221 <d2024007305+1@unifei.edu.br>
2025-03-26 08:56:31 -0300

    Update prot.py
    Comentarios adicionados

--- prot.py
+++ prot.py
@@ -1,6 +1,14 @@
+'''
+Nome: Caio Mendes Coutinho e Garcia
+Matricula: 2024007305
+Data: 26/03/2025
+'''
 import numpy as np
 import sys
 
+# Cria a matriz a partir da leitura dos dados do dataset (utiliza a funcao loadtxt da biblioteca numpy)
+# Em caso de sucesso, a funcao retorna uma ndarray, a depender das caracteristicas dos dados
+# Caso algum erro aconteça, None sera retornado 
 def criaMatriz(filename):
     try:
         return np.loadtxt(f"{filename}.txt")
@@ -10,7 +18,10 @@ def criaMatriz(filename):
     except ValueError as e:
         print(f"Erro encontrado: {e}")
         return None
-    
+
+# Salva o resultado da criaçao da matriz em um arquivo .txt, indicando qual foi o dataset e as dimensoes da ndarray criada
+# Em caso de sucesso, a funcao cria/modifica o arquivo corretamente
+# Caso algum erro aconteça, o usuario sera notificado  
 def salvarResultado(filename, lin, col):
     try:
         arq = open('resultado.txt', 'a+')
@@ -29,5 +40,6 @@ def main(filename):
     l, c = m.shape
     salvarResultado(filename, l, c)
 
+# Verifica se o script esta sendo executado diretamente como script ou como modulo em um externo
 if __name__ == '__main__':
     main(sys.argv[1])
